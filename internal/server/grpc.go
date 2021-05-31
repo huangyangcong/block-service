01f8fc758781eb7590ba6f8862f8763f31f6dc1f
huangyangcong <huangyangcong@chainfly.com>
2021-05-31 09:22:32 +0800

    init

---
+++ internal/server/grpc.go
@@ -0,0 +1,71 @@
+package server
+
+import (
+	v1 "block-service/api/helloworld/v1"
+	"block-service/internal/conf"
+	"block-service/internal/service"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/tracing"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/uber/jaeger-client-go"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/semconv"
+)
+
+// Get trace provider
+func tracerProvider(url string) (*tracesdk.TracerProvider, error) {
+	// Create the Jaeger exporter
+	exp, err := jaeger.NewRawExporter(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	tp := tracesdk.NewTracerProvider(
+		tracesdk.WithSampler(tracesdk.AlwaysSample()),
+		// Always be sure to batch in production.
+		tracesdk.WithBatcher(exp),
+		// Record information about this application in an Resource.
+		tracesdk.WithResource(resource.NewWithAttributes(
+			semconv.ServiceNameKey.String(pb.MessageService_ServiceDesc.ServiceName),
+			attribute.String("environment", "development"),
+			attribute.Int64("ID", 1),
+		)),
+	)
+	return tp, nil
+}
+
+// NewGRPCServer new a gRPC server.
+func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, logger log.Logger) *grpc.Server {
+	var opts = []grpc.ServerOption{
+		grpc.Middleware(
+			middleware.Chain(
+				recovery.Recovery(),
+				tracing.Server(
+					tracing.WithTracerProvider(otel.GetTracerProvider()),
+					tracing.WithPropagators(
+						propagation.NewCompositeTextMapPropagator(propagation.Baggage{}, propagation.TraceContext{}),
+					),
+				),
+				logging.Server(logger),
+			),
+		),
+	}
+	if c.Grpc.Network != "" {
+		opts = append(opts, grpc.Network(c.Grpc.Network))
+	}
+	if c.Grpc.Addr != "" {
+		opts = append(opts, grpc.Address(c.Grpc.Addr))
+	}
+	if c.Grpc.Timeout != nil {
+		opts = append(opts, grpc.Timeout(c.Grpc.Timeout.AsDuration()))
+	}
+	srv := grpc.NewServer(opts...)
+	v1.RegisterGreeterServer(srv, greeter)
+	return srv
+}
